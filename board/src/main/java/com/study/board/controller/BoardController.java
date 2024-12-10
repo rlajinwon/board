d1adb388a510836955f4cb0cf0e348cad6c2ea28
rlajinwon <129858521+rlajinwon@users.noreply.github.com>
2024-12-11 02:17:13 +0900

    Add files via upload

---
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,19 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController { //localhost:8080/board/write
+    @GetMapping("/board/write")
+    public String boardWriteFore(){
+        return "boardwrite";
+    }
+@PostMapping("/board/writepro")
+    public String BoardWritePro(Board board){
+    System.out.println(board.getTitle());
+    return "";
+    }
+}
