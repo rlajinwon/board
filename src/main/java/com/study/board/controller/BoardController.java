bc85f110b23bbc8ccc8474b48f3352a75c536a48
gimjin-won <wlsdnjs652@naver.com>
2024-12-11 01:59:20 +0900

    commit

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,7 +4,6 @@ import com.study.board.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController { //localhost:8080/board/write
